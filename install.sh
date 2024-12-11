8824b102679bf6d457908e74601ede9702b8525a
AKU CANTIK 💃 <hozooapi@gmail.com>
2024-12-11 22:05:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ if pgrep mpv > /dev/null; then
     echo "Musik sudah berjalan di latar belakang."
 else
     echo "Musik tidak sedang berjalan."
-    bash /data/data/com.termux/files/home/TOOLSV5/musik.sh
+    bash /data/data/com.termux/files/home/tes.sh/FLIPPERZERO-INDONESIA404.sh
+
 fi
